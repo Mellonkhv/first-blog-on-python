361d55bdcaa0c904b781d32366591f2f2c8fd673
mellonkhv@gmail.com <mellonkhv@gmail.com>
2017-01-24 14:20:04 +1000

    My Django Girls app, first commit

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('create_date', models.DateTimeField(default=datetime.datetime(2017, 1, 23, 6, 26, 52, 693960, tzinfo=utc))),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
